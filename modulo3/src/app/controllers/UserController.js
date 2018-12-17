a6f9a3856977953077342f5ff64dbc47eb935934
Moisés S. E. Aguilar <moisesmagno@gmail.com>
2018-12-17 11:10:49 -0200

    Criado as basses do modulo 3 de Node.js

---
+++ modulo3/src/app/controllers/UserController.js
@@ -0,0 +1,22 @@
+const { User } = require("../models/user");
+
+class UserController{
+
+    async store(req, res){
+        const { email } = req.body
+
+        if(await User.findOne({email})){
+            return res.status(400).json({
+                error: 'User already exists'
+            });
+        }
+
+        const user = User.create(req.body);
+
+        return res.json(user);
+    }
+
+}
+
+
+module.exports = new UserController();
