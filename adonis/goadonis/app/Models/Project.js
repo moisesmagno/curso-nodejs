fdf3bc3ee207cf865f82cedc7d534a68e7085480
Moisés S. E. Aguilar <moisesmagno@gmail.com>
2018-12-29 11:50:39 -0200

    Criado os relacionamentos - Adonis.js

--- adonis/goadonis/app/Models/Project.js
+++ adonis/goadonis/app/Models/Project.js
@@ -1,9 +1,20 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Project extends Model {
+
+  user () {
+    // Um projeto pertence a um usuário.
+    return this.belongsTo('App/Models/User')
+  }
+
+  tasks () {
+    // Um projeto pode ter várias tarefas.
+    return this.hasMany('App/Models/Task')
+  }
+
+
 }
 
 module.exports = Project
--- adonis/goadonis/app/Models/Task.js
+++ adonis/goadonis/app/Models/Task.js
@@ -4,6 +4,21 @@
 const Model = use('Model')
 
 class Task extends Model {
+
+  use () {
+    // Uma tarefa pertence a um usuário.
+    return this.belongsTo('App/Models/User')
+  }
+
+  project () {
+    // Uma tarefa pertence a um Projeto.
+    return this.belongsTo('App/Models/Project')
+  }
+
+  file () {
+    // Uma tarefa pertence a um Arquivo
+    return this.belongsTo('App/Models/File')
+  }
 }
 
 module.exports = Task
